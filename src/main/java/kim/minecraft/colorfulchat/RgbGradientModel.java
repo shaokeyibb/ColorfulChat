5d5b900c8dc4ae7130597ac1c4c74aa849bf7aa3
自然选择 <40818329+NaturalSelect@users.noreply.github.com>
2020-07-06 12:19:14 +0800

    Update RgbGradientModel.java

--- src/main/java/kim/minecraft/colorfulchat/RgbGradientModel.java
+++ src/main/java/kim/minecraft/colorfulchat/RgbGradientModel.java
@@ -9,7 +9,7 @@ public class RgbGradientModel {
 
     public static RgbGradientModel make(int r,int g,int b,int R,int G,int B) {
         Color begin = Color.fromRGB(r, g, b);
-        Color end = Color.fromBGR(R,G,B);
+        Color end = Color.fromRGB(R,G,B);
         RgbGradientModel model = new RgbGradientModel();
         model.begin = begin;
         model.end = end;
